55241e0876ac9b2f18e38f77c94e51f630c3e822
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-05-31 12:30:05 -0400

    Add files via upload

---
+++ loopreverse.js
@@ -0,0 +1,12 @@
+
+let classStudents = ["Samuel","Hannah","Grethel","Ben","Tanya","Art","Derrel","Camron"];
+
+//Output the student names in reverse order.
+
+
+for (let i = classStudents.length - 1; i  = 0; i--){
+
+    console.log(classStudents[i])}
+
+//For ( let i= 0; [where your gonna start the I]; I <classNames.length[ As long as it is less then the number in the array; I++[add one to i]){
+
